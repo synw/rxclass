a9cb492c1012bf80a31113ec9b2db0e049a5ec80
greg <greg@emencia.com>
2021-03-26 16:16:15 +0100

    Improve the RxRestClass

--- src/rest.ts
+++ src/rest.ts
@@ -1,43 +1,58 @@
-import { reactive, computed, ComputedRef } from "@vue/reactivity";
+import { reactive, ref } from "@vue/reactivity";
 import RxClass from "./base";
 
-function baseHeader(method: string): RequestInit { // eslint-disable-line
-  return {
-    method: method,
-    credentials: "include",
-    headers: {
-      "Content-Type": "application/json",
-    }
-  } as RequestInit;
-}
 
 export default abstract class RxRestClass extends RxClass {
-  private _dataset = reactive<Record<string, Array<any> | Record<string, any>>>({ data: {} }); // eslint-disable-line
+  public objectDataset = reactive<Record<string, any>>({}); // eslint-disable-line
+  public arrayDataset = reactive<Array<any>>([]); // eslint-disable-line
+  private _isLoading = ref<boolean>(false)
+
   public serverUrl: string;
 
-  constructor(serverUrl?: string, state: Record<string, any> = {}) {// eslint-disable-line
+  constructor(serverUrl?: string, state: Record<string, any> = {}) { // eslint-disable-line
     super(state)
     this.serverUrl = serverUrl ?? "";
   }
 
-  get dataset(): ComputedRef<Array<any> | Record<string, any>> {// eslint-disable-line
-    return computed(() => this._dataset.data)
+  get isLoading() {
+    return this._isLoading;
   }
 
-  async fetchData<T>(url: string) {
-    const response = await fetch(url, baseHeader("get"));
+  private get getHeader(): RequestInit {
+    return {
+      method: "get",
+      credentials: "include",
+      headers: {
+        "Content-Type": "application/json",
+      }
+    } as RequestInit;
+  }
+
+  async fetchGetObject(url: string): Promise<Record<string, any>> { // eslint-disable-line
+    this._isLoading = ref<boolean>(true);
+    const uri = this.serverUrl + url;
+    const response = await fetch(uri, this.getHeader);
+    //console.log("DATA", response)
     if (!response.ok) {
       throw new Error(response.statusText);
     }
-    const data = await response.json() as T;
-    this._dataset.data = data
+    const data = await response.json() as Record<string, any>; // eslint-disable-line
+    this.objectDataset = data;
+    this._isLoading = ref<boolean>(false);
+    return data;
   }
 
-  async fetchArrayData(url: string) {
-    await this.fetchData<Array<any>>(url); // eslint-disable-line
-  }
-
-  async fetchObjectData(url: string) {
-    await this.fetchData<Record<string, any>>(url); // eslint-disable-line
+  async fetchGetArray<T>(url: string): Promise<Array<T>> {
+    this._isLoading = ref<boolean>(true);
+    const uri = this.serverUrl + url;
+    const response = await fetch(uri, this.getHeader);
+    //console.log("DATA", response)
+    if (!response.ok) {
+      throw new Error(response.statusText);
+    }
+    const data = await response.json() as Array<T>; // eslint-disable-line
+    this.arrayDataset = data;
+    this._isLoading = ref<boolean>(false);
+    return data;
   }
 }
