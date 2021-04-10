3ad8e992de240bf1154385e1a3e80077955ede6c
greg <greg@emencia.com>
2021-04-10 22:07:48 +0200

    Export RxDebounced class

--- src/index.ts
+++ src/index.ts
@@ -2,5 +2,6 @@
 import RxClass from "./base";
 import RxStorageClass from "./persistence";
 import RxRestClass from "./rest";
+import RxDebounced from "./debounced";
 
-export { RxClass, RxStorageClass, RxRestClass }
+export { RxClass, RxStorageClass, RxRestClass, RxDebounced }
